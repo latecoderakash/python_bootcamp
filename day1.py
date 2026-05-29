3ca903a0f06d7465be7ae0b46f3469ed2a6e5865
Akash Kumar <akash.kumar@thalesgroup.com>
2026-05-29 12:58:57 +0530

    Initial Commit

---
+++ day1.py
@@ -0,0 +1,20 @@
+#print function
+# print("Hello World \nHello World")
+# print("Hello" + " World")
+
+
+#input functions
+# input("What is your name?")
+# print("Hello " + input("What is your name?") + "!")
+
+# variables
+# name = input("What is your name?")
+# length= (len(name))
+# print(length)
+
+# project 1: band name generator
+
+print("Welcome to the Band Name Generator")
+cityname = input("What's the name of your city you grew up? ")
+petname = input("What's your pet name? ")
+print ("Your band name could be " + cityname + " " + petname)
