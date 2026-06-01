a3c5829c521d1071897cab4f5a6f1493a98824b0
Akash Kumar <akash.kumar@thalesgroup.com>
2026-06-01 17:35:33 +0530

    day2 completed

---
+++ day2.py
@@ -0,0 +1,71 @@
+# print("hello"[0])
+# print("hello"[-1])
+# print("hello"[10])
+
+#length function
+# a= len("Hello")
+# print(a)
+
+# #type function
+# a= type("Hello")
+# print(a)
+
+#data types
+# a= type("Hello")
+# b = type(123)
+# c = type(345.56)
+# d = type(False)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+#type conversion
+# a = type(int("123"))
+# print(a)
+
+
+# print("Number of letters taken in your name: " + len(input("Enter you name ")))
+# print("Number of letters taken in your name: " + str(len(input("Enter you name "))))
+
+#mathmatical opeartor
+
+# print(123 + 456)
+# print(7-3)
+# print(2*3)
+# print(8/2)
+# print(type(8/2))
+# print(8//2)
+# print(2**5)
+
+# round opearator
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi))
+# print(round(bmi, 2))
+
+# assignment operator
+# score = 0
+# score = score + 1
+# print(score)
+
+# f strings
+
+score = 0
+height = 1.8
+is_winning = True
+
+# print(f"your score is = {score}, your height is = {height} and your winnig is {is_winning}")
+
+# project 2: Tip Calculator
+
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12 or 15?"))
+peoples= int(input("How many peope to split the bill? "))
+totalbill_after_tip= (total_bill * tip /100)
+per_person_bill = float((total_bill + totalbill_after_tip) / peoples)
+roundbill_two_places = round(per_person_bill, 2)
+print(f"Each person should pay: {roundbill_two_places}")
