a93db93888f9a91f10bdb560a47eb752a4786b1b
Akash Kumar <akash.kumar@thalesgroup.com>
2026-06-15 22:46:00 +0530

    day4 completed

---
+++ day4.py
@@ -0,0 +1,25 @@
+#random module
+
+# import random
+# number = random.randint(1,10)
+# print (number)
+
+# Heads or Tails
+# import random
+# random_value = random.randint(0,1)
+# if random_value == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+#Lists
+
+# state_of_india = ["UP", "UK"]
+# print(state_of_india)
+
+#bill pay game
+# import random
+# friends = ["a", "b", "c", "d"]
+# random_person_to_paybill = random.choice(friends)
+# print(random_person_to_paybill)
+
