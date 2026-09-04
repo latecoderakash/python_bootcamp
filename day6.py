040e9fc24ed3dc810f54b35a055fd63c8797003f
KUMAR Akash <T0250013@gemalto.com>
2026-09-04 13:23:02 +0530

    Day 6 Practice

---
+++ day6.py
@@ -0,0 +1,12 @@
+#creating function
+# def my_function():
+#     print("hello")
+#     print("bye")
+
+# my_function()
+
+#while loop
+
+# while something_is_true:
+#     #do this
+#     #then do this
